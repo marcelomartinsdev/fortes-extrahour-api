f5c6d00cc0727976f37edd863537bb5d4dfabe10
Marcelo Martins <marcelomartinsdev@gmail.com>
2024-04-03 16:05:40 -0300

    revert: remove Address Domain and Address Repository

--- src/main/java/com/example/fortesextrahourapi/repositories/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.fortesextrahourapi.repositories;
-
-import com.example.fortesextrahourapi.domain.Address;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends MongoRepository<Address,String> {
-}
