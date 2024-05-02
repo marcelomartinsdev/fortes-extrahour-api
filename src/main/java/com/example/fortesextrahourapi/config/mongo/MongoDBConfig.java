5d615a358bcc8abbfdc75b6b1112b69980540f0c
Marcelo Martins <marcelomartinsdev@gmail.com>
2024-05-02 19:56:36 -0300

    feat: create ExtraHoursProgramationServiceTest

--- src/main/java/com/example/fortesextrahourapi/config/mongo/MongoDBConfig.java
+++ src/main/java/com/example/fortesextrahourapi/config/mongo/MongoDBConfig.java
@@ -1,5 +1,6 @@
 package com.example.fortesextrahourapi.config.mongo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
@@ -8,14 +9,16 @@ import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 
 @Configuration
 public class MongoDBConfig {
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
 
     @Bean
-    public MongoDatabaseFactory mongoConfigure(){
-        return new SimpleMongoClientDatabaseFactory("mongodb://localhost:27017/extra-hours-management");
+    public MongoDatabaseFactory mongoConfigure() {
+        return new SimpleMongoClientDatabaseFactory(mongoUri);
     }
 
     @Bean
-    public MongoTemplate mongoTemplate(){
+    public MongoTemplate mongoTemplate() {
         return new MongoTemplate(mongoConfigure());
     }
 }
